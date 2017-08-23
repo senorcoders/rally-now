0c07f4ead34a837c396ecdd9676fe585ee2ad5f0
Milton Espinoza <=>
2017-08-22 18:34:46 -0600

    adding footer to edit profile

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { AlertsPage } from '../alerts/alerts'
+import { ProfilePage } from '../profile/profile'
+import { PopoverController } from 'ionic-angular';
+import { OverlayPage } from '../overlay/overlay'
 
 /**
  * Generated class for the EditProfilePage page.
@@ -15,11 +20,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EditProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public popoverCtrl: PopoverController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditProfilePage');
   }
+
+  goToOtherPage() {
+    
+       this.navCtrl.push(AlertsPage);
+     }
+   
+      goToProfile() {
+    
+       this.navCtrl.push(ProfilePage);
+     }
+   
+      presentPopover() {
+       let popover = this.popoverCtrl.create(OverlayPage);
+       popover.present();
+     }
 
 }
