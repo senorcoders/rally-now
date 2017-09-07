4ea7ee44c8ca9d1d8962a46be36742d1a0bf7382
Dayana <dayanalara95@gmail.com>
2017-09-07 17:44:07 -0600

    IOS update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,66 +4,109 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { FeedPage } from '../pages/feed/feed';
-import {
-  Push,
-  PushToken
-} from '@ionic/cloud-angular';
+// import {
+//   Push,
+//   PushToken
+// } from '@ionic/cloud-angular';
 
 
-//import { Push, PushObject, PushOptions } from '@ionic-native/push';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = FeedPage;
+    rootPage: any = FeedPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,  public alertCtrl: AlertController, public push: Push) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public alertCtrl: AlertController, private push: Push) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            statusBar.styleDefault();
+            splashScreen.hide();
+            this.pushsetup();
+
+          
+        });
+    }
+
       //this.pushsetup();
-      this.push.rx.notification()
-        .subscribe((msg) => {
-          alert(msg.title + ': ' + msg.text);
-      });
+            // to check if we have permission
+            // this.push.hasPermission()
+            //     .then((res: any) => {
+
+            //         if (res.isEnabled) {
+            //             console.log('We have permission to send push notifications');
+            //         } else {
+            //             console.log('We do not have permission to send push notifications');
+            //         }
+
+            //     });
+
+            // const options: PushOptions = {
+            //     android: {
+            //         senderID: '1017475098724'
+            //     },
+            //     ios: {
+            //         alert: 'true',
+            //         badge: true,
+            //         sound: 'false'
+            //     },
+            //     windows: {},
+            //     browser: {
+            //         pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+            //     }
+            // };
+
+            // const pushObject: PushObject = this.push.init(options);
+            // pushObject.on('notification').subscribe((notification: any) => {
+            //     if (notification.additionalData.foreground) {
+            //         let youralert = this.alertCtrl.create({
+            //             title: 'New Push notification',
+            //             message: notification.message
+            //         });
+            //         youralert.present();
+            //     }
+            // });
+
+            // pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+
+            // pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+
+            // pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+
+    pushsetup() {
+      const options: PushOptions = {
+       android: {
+           senderID: '1017475098724'
+       },
+       ios: {
+           alert: 'true',
+           badge: true,
+           sound: 'false'
+       },
+       windows: {}
+    };
+
+    const pushObject: PushObject = this.push.init(options);
+
+    pushObject.on('notification').subscribe((notification: any) => {
+      if (notification.additionalData.foreground) {
+        let youralert = this.alertCtrl.create({
+          title: 'New Push notification',
+          message: notification.message
+        });
+        youralert.present();
+      }
     });
-  }
 
-  // pushsetup() {
-  //   const options: PushOptions = {
-  //    android: {
-  //        senderID: '924920604639'
-  //    },
-  //    ios: {
-  //        alert: 'true',
-  //        badge: true,
-  //        sound: 'false'
-  //    },
-  //    windows: {}
-  // };
- 
-  // const pushObject: PushObject = this.push.init(options);
- 
-  // pushObject.on('notification').subscribe((notification: any) => {
-  //   if (notification.additionalData.foreground) {
-  //     let youralert = this.alertCtrl.create({
-  //       title: 'New Push notification',
-  //       message: notification.message
-  //     });
-  //     youralert.present();
-  //   }
-  // });
- 
-  // pushObject.on('registration').subscribe((registration: any) => {
-  //    //do whatever you want with the registration ID
-  // });
- 
-  // pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
-  // }
+    pushObject.on('registration').subscribe((registration: any) => {
+      console.log("registration", registration);
+       //do whatever you want with the registration ID
+    });
 
-  
+    pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
+    }
+
+
 }
-
