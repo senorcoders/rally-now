187e0e5e71ef79e415b41add3663b97408e18ad2
Milton Espinoza <=>
2017-09-07 13:30:28 -0600

    changing initial screen of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { FeedPage } from '../pages/feed/feed';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = FeedPage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,  public alertCtrl: AlertController) {
     platform.ready().then(() => {
