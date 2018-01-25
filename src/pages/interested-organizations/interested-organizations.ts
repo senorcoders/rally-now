d2b1142f283b20ca203ef427d000f26a024b2fec
Dayana <dayanalara95@gmail.com>
2018-01-25 16:42:33 -0600

    Updated Interested Organizations

--- src/pages/interested-organizations/interested-organizations.ts
+++ src/pages/interested-organizations/interested-organizations.ts
@@ -19,9 +19,14 @@ export class InterestedOrganizationsPage {
   enable:boolean = true;
   count:number= 0 ;
   buttonText:any = "Continue";
-  followArray:any = ["5242fadc-4a05-43f3-afa8-9a421984c281", "c638f9c4-4a81-48d9-8664-6acd10058eaf", "e233339d-8f0a-405f-be52-b9a3d1d09b72",
-  "f2b1f2dc-2b66-4412-b5e0-57aea34fb040", "993a6240-849b-4cc8-90b0-bd7574570430", "9ff402a5-daf1-45c7-9f50-adcb899649e4",
-  "fa55c654-8396-42c1-aefa-d69283719936", "fd79e7bb-6cea-487b-8a7f-c6f5194b8ec9", "81b79228-bb12-4b95-81b2-604bcfa50548"];
+  followArray:any = ["5f4231c9-c362-4663-b5fa-8bae8c3831f1", "2f20250a-8cd2-493a-9a4c-3f01c8330a92", "cf89151e-5ab0-4a1a-bb60-95035c089a52",
+  "13421842-df20-4ebc-b191-89d0d4fe8094", "5d6ba8d7-ba1e-453c-ad91-a8a5de210100", "178daa51-5731-453b-b9fe-3b31ec93eb28",
+  "25109fc7-5c51-4fb7-bf47-d81ec80fa08f", "98c9bc19-dbeb-49cb-9cb5-f744db0738ac", "fd79e7bb-6cea-487b-8a7f-c6f5194b8ec9",
+  "6905e335-dfa7-4865-bd98-f32cec4aa340", "0237382c-f9b0-46fa-aab5-6d383f7813e4", "df5d9984-ebfe-423b-8d71-23cc86a16ad9", "26a234ad-ada5-41e5-bcc8-14452bbd528b",
+  "b7e8b91a-103a-40eb-b6a0-e2f1a7066008", "acb84ea8-1dca-4e4c-8093-6c04827f2539", "fb68fe1b-0e40-47ce-a0ea-73570ee1d852", "d034ff40-0fde-41f0-9868-330327a0e22d",
+  "3d3a6fc4-0a44-4421-a5c6-438ee94e7fa0", "7069eca3-9997-4af5-9e51-03c51b8add68", "801ff562-fe96-43e5-b3d0-1ee19f1c48cc", "651f2dca-957f-43ed-97a5-88efb8b58268",
+  "bf833c81-4930-48ed-8e94-8bf0d01fbbe9", "f4e14dbb-4172-4b5a-9558-d82fca5f1f3c", "b50ee250-3d9e-4bd9-877f-66dcf8d3aeb7", "c57d61d4-6f34-4b4a-a10a-f8ade9a8d111",
+  "aa06299b-2bc3-4c04-9f8e-7058421a92a0"];
   organizationEndpoint:any = 'following_organizations';
   myrallyID:any;
   
@@ -35,7 +40,7 @@ export class InterestedOrganizationsPage {
     public userData: UserData
     ) {
       console.log(ORG);
-      this.count = 9;
+      this.count = this.followArray.length;
       this.buttonText = "Follow (" + this.count + ")";
       console.log(this.followArray);
 
